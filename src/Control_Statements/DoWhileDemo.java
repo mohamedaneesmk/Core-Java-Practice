ae0b87da852854ffed446c2e10fbe92ad3713037
mohamedaneesmk <21ec267@students.stjosephs.ac.in>
2025-12-20 10:06:32 +0530

    Do While Demo

---
+++ src/Control_Statements/DoWhileDemo.java
@@ -0,0 +1,13 @@
+package Control_Statements;
+
+public class DoWhileDemo {
+    public static void main(String[] args) {
+
+        int i = 1;
+
+        do {
+            System.out.println("i = " + i);
+            i++;
+        } while (i <= 5);
+    }
+}
