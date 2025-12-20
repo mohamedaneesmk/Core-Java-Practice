74dc384fadfbb03346ac21e3286e9f192d737c15
mohamedaneesmk <21ec267@students.stjosephs.ac.in>
2025-12-20 10:04:27 +0530

    For Loop Demo

---
+++ src/Control_Statements/ForLoopDemo.java
@@ -0,0 +1,10 @@
+package Control_Statements;
+
+public class ForLoopDemo {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}
