d07b1ade052fc516d2a43888d5eafe67afcfaf43
mohamedaneesmk <21ec267@students.stjosephs.ac.in>
2025-12-20 10:01:20 +0530

    If Demo

---
+++ src/Control_Statements/IfDemo.java
@@ -0,0 +1,12 @@
+package Control_Statements;
+
+public class IfDemo {
+    public static void main(String[] args) {
+
+        int age = 20;
+
+        if (age >= 18) {
+            System.out.println("Eligible to vote");
+        }
+    }
+}
